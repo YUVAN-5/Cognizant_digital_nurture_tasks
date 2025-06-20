93b263413c7afc5a1a44536dbfb07e8ab5ea7662
YUVAN SHANKAR <yuvanganesan4325@gmail.com>
2025-06-20 14:45:23 +0530

    factorypattern done

---
+++ week_1/Design_principles&Patterns/FactoryPattern/FactoryPatternDemo.java
@@ -0,0 +1,13 @@
+
+public class FactoryPatternDemo {
+    public static void main(String[] args) {
+        Notification n1 = NotificationFactory.createNotification("EMAIL");
+        n1.notifyUser();
+
+        Notification n2 = NotificationFactory.createNotification("SMS");
+        n2.notifyUser();
+
+        Notification n3 = NotificationFactory.createNotification("PUSH");
+        n3.notifyUser();
+    }
+}
